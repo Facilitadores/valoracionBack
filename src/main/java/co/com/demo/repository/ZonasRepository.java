49e5f8a09ed5a0c48f4756a430af4aa1c3c81004
unknown <admin@PFORFR3B.itcol.com>
2019-06-07 16:23:40 -0500

    eliminacion proyecto sebas

--- src/main/java/co/com/demo/repository/ZonasRepository.java
+++
@@ -1,9 +0,0 @@
-package co.com.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.com.demo.model.Zonas;
-
-public interface ZonasRepository extends JpaRepository<Zonas,String>{
-
-}
