ddde169ea28f9b16ff50f7171d2cd9d88cb2a576
unknown <admin@PFORFR3B.itcol.com>
2019-06-07 17:37:42 -0500

    revision de error

--- src/main/java/co/com/demo/model/Facilitador.java
+++ src/main/java/co/com/demo/model/Facilitador.java
@@ -4,15 +4,18 @@ package co.com.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity //tablas
 @Table(name="celulas_facilitadores")
-public class Facilitador {
+public class Facilitador implements Serializable{
+    private static final long serialVersionUID = 1L;
 
     @Id
     private int id;
     private String celula;
     private String facilitador;
+    private String fullstack;
 
     public Facilitador() {
 
@@ -40,5 +43,13 @@ public class Facilitador {
 
     public void setFacilitador(String facilitador) {
         this.facilitador = facilitador;
+    }
+
+    public String getFullstack() {
+        return fullstack;
+    }
+
+    public void setFullstack(String fullstack) {
+        this.fullstack = fullstack;
     }
 }
